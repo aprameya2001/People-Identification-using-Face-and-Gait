7165d54313c52b8aedbfe30646ebfaaf86bedc56
spectre900 <nayakpratham0@gmail.com>
2022-03-20 21:23:29 +0530

    add notebooks

--- extract.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.system('mkdir extracted_dataset')
-
-files = os.listdir('dataset')
-
-for file in files:
-    os.system('unzip dataset/' + file)
-    os.system('mv ' + file.split('.')[0] + ' extracted_dataset/' + file.split('.')[0])
